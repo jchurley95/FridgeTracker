7af8726e7b851065c041197b655c3acab4604bcf
jchurley95 <jchurley95@gmail.com>
2017-08-25 00:17:37 -0400

    Option E create a food in a fridge works

--- lib/fridge_tracker.rb
+++ lib/fridge_tracker.rb
@@ -22,9 +22,7 @@ puts "You selected #{option}"
 
 ### Option A) List all Fridges
 if option.upcase == 'A'
-
   puts Fridge.all.to_s
-
   Fridge.all.map do |fridge|
     puts "Fridge ID = " + fridge.id.to_s
     puts fridge.location.to_s
@@ -32,22 +30,17 @@ if option.upcase == 'A'
     puts fridge.size.to_s
     puts "___ ___ ___"
   end
-
 end
 ### END Option A
 
 ### Option B) Add A Fridge
 if option.upcase == 'B'
-
   puts "What is this fridge's location?"
   Fridge_location = gets.chomp 
-
   puts "What is brand"
   Fridge_brand = gets.chomp   
-
   puts "What is size"
   Fridge_size = gets.chomp 
-
   Fridge.create do |fridge|
     fridge.location = Fridge_location
     fridge.brand = Fridge_brand
@@ -55,41 +48,36 @@ if option.upcase == 'B'
     fridge.has_food = false
     fridge.has_drink = false 
   end 
-
 end
 ### END Option B
 
 ### Option C) Delete A Fridge
 if option.upcase == 'C'
-
   #List Fridge Options
   Fridge.all.map do |fridge|
     puts "Fridge ID = " + fridge.id.to_s
     puts fridge.location.to_s
     puts "___ ___ ___"
   end
-
   puts "Enter The ID Of A Fridge to Delete"
   fridge_id_to_delete = gets.chomp
   Fridge.destroy(fridge_id_to_delete)
-
 end
 ### END Option C
 
 ### Option D) List All Food In A Fridge Of Your Choice
 if option.upcase == 'D'
-
-  #List Fridge Options
+  # List Fridge Options
   Fridge.all.map do |fridge|
     puts "Fridge ID = " + fridge.id.to_s
     puts fridge.location.to_s
     puts "___ ___ ___"
   end
-
+  # Enter ID of fridge
   puts "Enter The ID Of A Fridge"
   fridge_to_look_in_id = gets.chomp
   fridge_to_look_in = Fridge.find(fridge_to_look_in_id)
-  
+  #Display those Foods in that Fridge
   fridge_to_look_in.food.map do |food|
     puts "Looks like we got a(n) " + food.name.to_s
     puts "I'd reckon it's about " + food.weight.to_s + " pounds"
@@ -97,52 +85,75 @@ if option.upcase == 'D'
     puts "Looks like it's been around since " + food.created_at.to_s
     puts "___ ___ ___"
   end
-
 end
 ### END Option D
 
 ### Option E) Add A Food Item To A Fridge Of Your Choice
 if option.upcase == 'E'
-
-  puts "Enter The ID Of A Fridge"
-
   #List Fridge Options
   Fridge.all.map do |fridge|
     puts "Fridge ID = " + fridge.id.to_s
     puts fridge.location.to_s
     puts "___ ___ ___"
   end
-
+  # Enter ID of fridge
+  puts "Enter The ID Of A Fridge"
+  fridge_to_look_in_id = gets.chomp
+  fridge_to_look_in = Fridge.find(fridge_to_look_in_id)
+  # Create Food
+  puts "What is this food's name?"
+    Food_name = gets.chomp 
+  puts "How much does this food weigh?"
+    Food_weight = gets.chomp   
+  puts "Is this vegan? (Enter 'true' or 'false')"
+    Food_vegan = gets.chomp 
+    # if Food_vegan = "Y" do 
+    #   Food_vegan = true
+    # elsif Food_vegan = "N" do 
+    #   Food_vegan = false
+    # end
+  fridge_to_look_in.food.create do |food|
+    food.name = Food_name
+    food.weight = Food_weight
+    food.is_vegan = Food_vegan
+  end
 end
 ### END Option E
 
 ### Option F) Eat (Delete) A Food Item From A Fridge Of Your Choice
 if option.upcase == 'F'
-
-  puts "Enter The ID Of A Fridge"
-
   #List Fridge Options
   Fridge.all.map do |fridge|
     puts "Fridge ID = " + fridge.id.to_s
     puts fridge.location.to_s
     puts "___ ___ ___"
   end
-
+  puts "Enter The ID Of A Fridge to Delete"
+  fridge_id_to_delete = gets.chomp
+  Fridge.food.find().destroy(fridge_id_to_delete)
 end
 ### END Option F
 
 ### Option G) List All Drinks in A Fridge Of Your Choice
 if option.upcase == 'G'
-
-  puts "Enter The ID Of A Fridge"
-
-  #List Fridge Options
+  # List Fridge Options
   Fridge.all.map do |fridge|
     puts "Fridge ID = " + fridge.id.to_s
     puts fridge.location.to_s
     puts "___ ___ ___"
   end
-
+  # Enter ID of fridge
+  puts "Enter The ID Of A Fridge"
+  fridge_to_look_in_id = gets.chomp
+  fridge_to_look_in = Fridge.find(fridge_to_look_in_id)
+  #Display those Foods in that Fridge
+  fridge_to_look_in.drink.map do |drink|
+    puts "Looks like we got a(n) " + drink.name.to_s
+    puts drink.size.to_s + " ounces " 
+    puts "Alcohol? " + drink.is_alcoholic.to_s
+    puts "Looks like it's been around since " + drink.created_at.to_s
+    puts "___ ___ ___"
+  end
 end
 ### END Option G
 
